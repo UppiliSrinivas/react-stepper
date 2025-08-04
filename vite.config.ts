ce604a99c105639fdc5ebfe6884ef4de14209fed
UppiliSrinivas <uppilisrinivasanecs@gmail.com>
2025-08-04 13:39:37 +0530

    vite dts package added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import dts from 'vite-plugin-dts';
 import * as path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),dts({
+      insertTypesEntry: true,
+    }),],
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "./src") },
     ],
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.tsx'),
+      name: 'MultiStepper',
+      fileName: (format) => `multi-stepper.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
   },
 })
