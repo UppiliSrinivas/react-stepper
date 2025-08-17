c619588484e55c55f64bc8e94377f5cce0b3d4f8
UppiliSrinivas <uppilisrinivasanecs@gmail.com>
2025-08-17 12:34:20 +0530

    created vitest file

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// import tailwindcss from '@tailwindcss/vite'
-import dts from 'vite-plugin-dts';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 import * as path from "path";
-import {dependencies} from './package.json'
-import {libInjectCss} from 'vite-plugin-lib-inject-css';
-// https://vite.dev/config/
+import { dependencies } from "./package.json";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 export default defineConfig({
-  plugins: [react(),libInjectCss(),dts({
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
       insertTypesEntry: true,
-    }),],
-   test: {
-    globals: true,           // Use describe, it, expect without imports
-    environment: 'jsdom',    // Use jsdom for DOM APIs
-    setupFiles: './src/setupTests.ts',
-  }, 
+    }),
+  ],
+
   resolve: {
-    alias: [
-      { find: "@", replacement: path.resolve(__dirname, "./src") },
-    ],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
   build: {
-      lib: {
-        entry: path.resolve(__dirname, "src/index.ts"),
-        name: "multi-stepper",
-        fileName: (format) => `multi-stepper.${format}.js`,
-      },
-      rollupOptions: {
-        external: Object.keys(dependencies),
-        output: {
-          globals: {
-            react: "React",
-            "react-dom": "ReactDOM",
-          },
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "multi-stepper",
+      fileName: (format) => `multi-stepper.${format}.js`,
+    },
+    rollupOptions: {
+      external: Object.keys(dependencies),
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
-      cssCodeSplit: false,
-      sourcemap: false,
-      emptyOutDir: true,
-       minify: 'esbuild'
     },
-    
-})
+    cssCodeSplit: false,
+    sourcemap: false,
+    emptyOutDir: true,
+    minify: "esbuild",
+  },
+});
